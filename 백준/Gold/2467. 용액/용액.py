482ad9da66c15f4ccec4454d364f564dddbd297f
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-07-10 21:10:39 +0900

    Update 용액.py
    [Gold V] Title: 용액, Time: 184 ms, Memory: 127308 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -8,19 +8,10 @@ arr = list(map(int, input().split()))
 min_val = maxsize
 min_a, min_b = -maxsize, maxsize
 for i in range(n - 1):
-    r_idx = bisect_left(arr, -arr[i], lo=i + 1)
-    if r_idx >= n:
-        r_idx -= 1
-    if r_idx == i:
-        continue
-    if abs(arr[i] + arr[r_idx]) < min_val:
-        min_val = abs(arr[i] + arr[r_idx])
-        min_a, min_b = arr[i], arr[r_idx]
-    if r_idx > 0:
-        r_idx -= 1
-    if r_idx == i:
-        continue
-    if abs(arr[i] + arr[r_idx]) < min_val:
-        min_val = abs(arr[i] + arr[r_idx])
-        min_a, min_b = arr[i], arr[r_idx]
+    right = bisect_left(arr, -arr[i], lo=i + 1)
+    for r_idx in (right - 1, right):
+        if i < r_idx < n:
+            if abs(arr[i] + arr[r_idx]) < min_val:
+                min_val = abs(arr[i] + arr[r_idx])
+                min_a, min_b = arr[i], arr[r_idx]
 print(min_a, min_b)
