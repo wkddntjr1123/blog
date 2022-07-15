db61d929e8f5ea567178876013cb24ec485ceb21
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-07-15 16:47:41 +0900

    [Silver I] Title: 1로 만들기 2, Time: 144 ms, Memory: 130120 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,35 +1,31 @@
-from collections import deque
-from sys import stdin
+from sys import maxsize, stdin
 
-input = stdin.readline
+input = lambda: stdin.readline().rstrip()
 
-x = int(input().rstrip())
-route = [False for _ in range(x + 1)]
+x = int(input())
+dp = [maxsize] * (x + 3)
+parent = [-1] * (x + 3)
+dp[1] = 0
+dp[2] = 1
+parent[2] = 1
+dp[3] = 1
+parent[3] = 1
 
-
-def bfs(num):
-    q = deque()
-    q.append((num, 0))
-    while q:
-        num, level = q.popleft()
-        if num == 1:
-            return level
-        if num % 3 == 0 and route[num // 3] == False:
-            q.append((num // 3, level + 1))
-            route[num // 3] = num
-        if num % 2 == 0 and route[num // 2] == False:
-            q.append((num // 2, level + 1))
-            route[num // 2] = num
-        if num - 1 >= 1 and route[num - 1] == False:
-            q.append((num - 1, level + 1))
-            route[num - 1] = num
-
-
-min_level = bfs(x)
-print(min_level)
-start = 1
-result = []
-while start:
-    result.append(start)
-    start = route[start]
-print(" ".join(map(str, result[::-1])))
+for i in range(4, x + 1):
+    val = maxsize
+    if i % 3 == 0:
+        val = dp[i // 3]
+        parent[i] = i // 3
+    if i % 2 == 0 and dp[i // 2] < val:
+        val = dp[i // 2]
+        parent[i] = i // 2
+    if dp[i - 1] < val:
+        val = dp[i - 1]
+        parent[i] = i - 1
+    dp[i] = val + 1
+print(dp[x])
+route = [x]
+while parent[x] != -1:
+    route.append(parent[x])
+    x = parent[x]
+print(*route)
