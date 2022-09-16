1b1e8f0ecea8d319c7775e9ca52721347eb9e26f
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-16 18:43:59 +0900

    [Silver II] Title: 에디터, Time: 280 ms, Memory: 129632 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,85 +1,23 @@
+from collections import deque
 from sys import stdin
 
 input = lambda: stdin.readline().rstrip()
 
+left, right = deque(input()), deque()
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-class DoubleLinkedList:
-    def __init__(self, s):
-        self.head = None
-        self.cursor = None
-
-        for char in s:
-            newNode = Node(char)
-            if not self.head:
-                self.head = newNode
-                self.cursor = newNode
-                continue
-            newNode.left = self.cursor
-            self.cursor.right = newNode
-            self.cursor = newNode
-        self.tail = Node(None)
-        self.tail.left = newNode
-        newNode.right = self.tail
-        self.cursor = self.tail
-
-    def L(self):
-        if self.cursor is self.head:
-            return
-        self.cursor = self.cursor.left
-
-    def D(self):
-        if self.cursor is self.tail:
-            return
-        self.cursor = self.cursor.right
-
-    def B(self):
-        if self.cursor is self.head:
-            return
-        delNode = self.cursor.left
-        if delNode is not self.head:
-            self.cursor.left = delNode.left
-            delNode.left.right = self.cursor
-        else:
-            self.head = self.cursor
-        del delNode
-
-    def P(self, value):
-        newNode = Node(value)
-        if self.cursor is not self.head:
-            prevNode = self.cursor.left
-            prevNode.right = newNode
-            newNode.left = prevNode
-        else:
-            self.head = newNode
-        self.cursor.left = newNode
-        newNode.right = self.cursor
-
-    def stringify(self):
-        cur = self.head
-        result = []
-        while cur.value:
-            result.append(cur.value)
-            cur = cur.right
-        return "".join(result)
-
-
-linkedList = DoubleLinkedList(input())
 for _ in range(int(input())):
     op = input()
     if op == "L":
-        linkedList.L()
+        if left:
+            right.appendleft(left.pop())
     elif op == "D":
-        linkedList.D()
+        if right:
+            left.append(right.popleft())
     elif op == "B":
-        linkedList.B()
+        if left:
+            left.pop()
     else:
         _, value = op.split()
-        linkedList.P(value)
-print(linkedList.stringify())
+        left.append(value)
+
+print("".join(left) + "".join(right))
