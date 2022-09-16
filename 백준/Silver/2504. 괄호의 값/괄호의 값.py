e0569d8bbdf130cb9726a949c5abecd71aaf990d
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-16 21:11:47 +0900

    [Silver I] Title: 괄호의 값, Time: 68 ms, Memory: 30872 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -1,36 +1,34 @@
-from sys import stdin
-
-input = stdin.readline
-
-s = input().rstrip()
-
-
-def cal_s(s):
+def process(s: str) -> int:
     stack = []
-    match = {")": "(", "]": "["}
-    result = 0
-    if s[0] in (")", "]"):
+    table = {")": "(", "]": "["}
+    # 첫문자가 잘못된 경우 return 0
+    if (not s) or (s[0] in table):
         return 0
+    result = 0
     for i in range(len(s)):
+        # 여는 괄호면 stack에 추가
         if s[i] in ("(", "["):
             stack.append(s[i])
+        # 닫는 괄호면
         else:
-            if not stack or stack.pop() != match[s[i]]:
+            # "())))))))"같은 오른쪽 괄호가 더 많은 잘못된 케이스 입력 예외처리
+            if not stack:
                 return 0
-            elif s[i - 1] in (")", "]"):
+            last = stack.pop()
+            # 괄호 짝이 잘못된 경우 return 0 : 스택에서 꺼낸 아이템과 s[i]가 다르면 괄호에 문제가 있다는 의미
+            if table[s[i]] != last:
+                return 0
+            # 괄호 짝이 맞는데 이전 문자가 아닌 경우 = (x), [x] 인 경우
+            if s[i - 1] != table[s[i]]:
                 continue
+            # 괄호 짝이 맞고 이전 문자도 동일한 경우 = (), [] 인 경우
             else:
-                num_2, num_3 = 0, 0
-                for char in stack:
-                    if char == "(":
-                        num_2 += 1
-                    else:
-                        num_3 += 1
-                if s[i] == ")":
-                    result += 2 * (2**num_2) * (3**num_3)
-                else:
-                    result += 3 * (2**num_2) * (3**num_3)
+                # ()면 2, []면 3
+                num = 2 if s[i] == ")" else 3
+                # 스택에 있는 "("는 x2, "["는 x3
+                result += num * (2 ** stack.count("(")) * (3 ** stack.count("["))
+    # 최종적으로 스택에 값이 없어야 정상
     return result if not stack else 0
 
-
-print(cal_s(s))
+s = input()
+print(process(s))
