c48dde7e0cd06a8d54473ddd1f6846d351bc106f
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-16 18:38:11 +0900

    [Silver II] Title: 에디터, Time: 312 ms, Memory: 197940 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,43 +1,85 @@
-from collections import deque
 from sys import stdin
 
-input = stdin.readline
-s = input().rstrip()
-num_op = int(input())
+input = lambda: stdin.readline().rstrip()
 
-left = deque()
-right = deque()
-for char in s:
-    left.append(char)
 
-def move_start():
-    global left, right
-    if left:
-        right.appendleft(left.pop())
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-def move_end():
-    global left, right
-    if right:
-        left.append(right.popleft())
 
-def insert_data(data):
-    global left
-    left.append(data)
+class DoubleLinkedList:
+    def __init__(self, s):
+        self.head = None
+        self.cursor = None
 
-def delete_data():
-    global left
-    if left:
-        left.pop()
+        for char in s:
+            newNode = Node(char)
+            if not self.head:
+                self.head = newNode
+                self.cursor = newNode
+                continue
+            newNode.left = self.cursor
+            self.cursor.right = newNode
+            self.cursor = newNode
+        self.tail = Node(None)
+        self.tail.left = newNode
+        newNode.right = self.tail
+        self.cursor = self.tail
 
-for _ in range(num_op):
-    op = input().split()  # L:좌 ,D:우, B:삭제, P는 삽입
-    if op[0] == "P":
-        insert_data(op[1])
-    elif op[0] == "L":
-        move_start()
-    elif op[0] == "D":
-        move_end()
+    def L(self):
+        if self.cursor is self.head:
+            return
+        self.cursor = self.cursor.left
+
+    def D(self):
+        if self.cursor is self.tail:
+            return
+        self.cursor = self.cursor.right
+
+    def B(self):
+        if self.cursor is self.head:
+            return
+        delNode = self.cursor.left
+        if delNode is not self.head:
+            self.cursor.left = delNode.left
+            delNode.left.right = self.cursor
+        else:
+            self.head = self.cursor
+        del delNode
+
+    def P(self, value):
+        newNode = Node(value)
+        if self.cursor is not self.head:
+            prevNode = self.cursor.left
+            prevNode.right = newNode
+            newNode.left = prevNode
+        else:
+            self.head = newNode
+        self.cursor.left = newNode
+        newNode.right = self.cursor
+
+    def stringify(self):
+        cur = self.head
+        result = []
+        while cur.value:
+            result.append(cur.value)
+            cur = cur.right
+        return "".join(result)
+
+
+linkedList = DoubleLinkedList(input())
+for _ in range(int(input())):
+    op = input()
+    if op == "L":
+        linkedList.L()
+    elif op == "D":
+        linkedList.D()
+    elif op == "B":
+        linkedList.B()
     else:
-        delete_data()
-
-print(*left, *right, sep="")
+        _, value = op.split()
+        linkedList.P(value)
+print(linkedList.stringify())
