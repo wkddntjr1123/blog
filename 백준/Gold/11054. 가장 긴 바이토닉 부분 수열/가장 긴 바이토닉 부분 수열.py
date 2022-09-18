6441e2eefeba1050ac7be42a7d03dbfa483d84cf
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-18 20:04:34 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 128 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,26 +1,28 @@
 from sys import stdin
 
-input = lambda: stdin.readline().rstrip()
+input = stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1] * n  # dp[i] : i번째 수 포함 시, 오름차순 최장 부분 수열 길이
-r_dp = [1] * n  # r_dp[i] : i번째 수 포함 시, 내림차순 최장 부분 수열 길이 => arr 뒤집어서 계산
 
-for i in range(1, n):
-    for j in range(i):
-        if arr[i] > arr[j] and dp[i] < dp[j] + 1:
-            dp[i] = dp[j] + 1
+def getAscTable(arr):
+    table = [1 for _ in range(n)]
+    for i in range(1, len(arr)):
+        maxVal = 0
+        for j in range(i):
+            if arr[j] < arr[i]:
+                maxVal = max(maxVal, table[j])
+        table[i] = maxVal + 1
+    return table
 
+
+table = getAscTable(arr)
 arr.reverse()
-for i in range(1, n):
-    for j in range(i):
-        if arr[i] > arr[j] and r_dp[i] < r_dp[j] + 1:
-            r_dp[i] = r_dp[j] + 1
+reverseTable = getAscTable(arr)
+reverseTable.reverse()
 
 answer = 0
 for i in range(n):
-    answer = max(answer, dp[i] + r_dp[n - i - 1] - 1)
-
+    answer = max(answer, table[i] + reverseTable[i] - 1)
 print(answer)
