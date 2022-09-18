58b12b8d0923eb4ebd0f5f74cbe4300585aae2ff
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-18 19:53:58 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 120 ms, Memory: 114408 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -1,12 +1,15 @@
 from sys import stdin
 
-input = lambda: stdin.readline().rstrip()
+input = stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [1] * n
+table = [1 for _ in range(n)]
+
 for i in range(1, n):
-    for j in range(i):
-        if arr[i] < arr[j] and dp[i] < dp[j] + 1:
-            dp[i] = dp[j] + 1
-print(max(dp))
+    maxVal = 0
+    for j in range(0, i):
+        if arr[i] < arr[j]:
+            maxVal = max(maxVal, table[j])
+    table[i] = maxVal + 1
+print(max(table))
