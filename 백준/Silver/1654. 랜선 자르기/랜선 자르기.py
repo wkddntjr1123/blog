e52979830157e1fd7d509fdb48b295e2de1c47fb
장우석 <64186072+wkddntjr1123@users.noreply.github.com>
2022-09-19 22:53:48 +0900

    [Silver II] Title: 랜선 자르기, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,31 +1,24 @@
 from sys import stdin
 
 input = stdin.readline
-k, n = map(int, input().split())
+
+k, target = map(int, input().split())
 lines = [int(input()) for _ in range(k)]
 
-# 최대 길이 라인
-max_line_len = max(lines)
+
+def getCount(lines, length):
+    return sum(map(lambda line: line // length, lines))
 
 
-def need_more_line(mid):
-    res = 0
-    for line in lines:
-        res += line // mid
-    if res < n:  # 랜선 더 필요
-        return True
-    else:  # 랜선이 오히려 많거나 현재 딱 맞으면
-        return False
+def biLeft(lines, target):
+    lo, hi = 1, max(lines)
+    while lo <= hi:
+        cutLength = (lo + hi) // 2
+        if target > getCount(lines, cutLength):
+            hi = cutLength - 1
+        else:
+            lo = cutLength + 1
+    return hi
 
 
-def find_max_len(left, right):
-    mid = (left + right) // 2
-    # 최대길이 도달
-    if left == mid:
-        return mid
-    if need_more_line(mid):
-        return find_max_len(left, mid)
-    else:
-        return find_max_len(mid, right)
-# 시작부터 최대길이로 정답이 나오는 경우 예외처리
-print(max_line_len) if not need_more_line(max_line_len) else print(find_max_len(left=1, right=max_line_len))
+print(biLeft(lines, target))
